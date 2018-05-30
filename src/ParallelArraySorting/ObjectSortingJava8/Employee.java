12aba73310aa6af029304349820a8298d7ca7d1a
geoff <geoff.seloana@gmail.com>
2018-05-31 00:48:58 +0200

    Feature

--- src/ParallelArraySorting/ObjectSortingJava8/Employee.java
+++ src/ParallelArraySorting/ObjectSortingJava8/Employee.java
@@ -1,4 +1,19 @@
 package ParallelArraySorting.ObjectSortingJava8;
 
+import org.junit.Before;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Employee {
+
+    private Integer employeeNumber;
+    private String employeeFirstName;
+    private String employeeLastName;
+    private LocalDate hireDate;
+
+    //..
 }
+
